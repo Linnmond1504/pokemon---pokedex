0ba6e4a65e05140049df61bc868fd786f2fb80d8
Linnmond1504 <linnett0207@gmail.com>
2025-07-25 02:18:25 -0300

    Fix rutas para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/pokemon---pokedex/',
   plugins: [react()],
-  base: '/pokemon---pokedex/', // el nombre del repo va entre barras
 })
