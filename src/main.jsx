ce34336dcbdaa17c09019b5991ac5c67a1b486d3
Linnmond1504 <linnett0207@gmail.com>
2025-07-25 02:26:17 -0300

    Agrego basename para React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'  // <-- Importá BrowserRouter
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/pokemon---pokedex"> {/* <-- Agregá basename */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
